461cb870f5662cb6a338c2468e48972884f99fdb
GitHub Action <action@github.com>
2021-12-07 07:15:38 +0000

    Template cleanup

--- src/main/kotlin/com/github/jummerylee/mvvmfragment/MyBundle.kt
+++ src/main/kotlin/com/github/jummerylee/mvvmfragment/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.jummerylee.mvvmfragment
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/jummerylee/mvvmfragment/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/jummerylee/mvvmfragment/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.jummerylee.mvvmfragment.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.jummerylee.mvvmfragment.MyBundle
 
 class MyApplicationService {
 
